b6b582737b85bab4138c96c5025197ec14b3adb5
Gerardo Manuel Chávez Larios <gerardo.larios69@gmail.com>
2020-06-14 18:08:24 -0500

    [REF] init database

--- src/schema.py
+++ src/schema.py
@@ -1,12 +1,21 @@
-from sqlalchemy import Table, Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Table, Column, Integer, String, Date, UniqueConstraint
 
-def set_tables(meta):
-    tradres = Table('trades', meta,
-                    Column('id', Integer, primary_key=True),
-                    Column('book', String),
-                    Column('maker_side', String),
-                    Column('amount', String),
-                    Column('price', String),
-                    Column('created_at', Date))
-    return [trades]
+Base = declarative_base()
+
+class Trade(Base):
+    __tablename__ = 'trades'
+
+    id = Column(Integer, primary_key=True, unique=True)
+    book = Column(String)
+    maker_side = Column(String)
+    amount = Column(String)
+    price = Column(String)
+    created_at = Column(String)
+
+    def __repr__(self):
+        return f'{self.maker_side} : {self.amount} {self.book} * ${self.price} at {self.created_at}'
+
+def create_all_tables(engine):
+    Base.metadata.create_all(engine, checkfirst=True)
 
