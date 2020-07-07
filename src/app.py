a75da2ff5b595d96beeaa66dce553f9785318f94
Gerardo Manuel Chávez Larios <gerardo.larios69@gmail.com>
2020-07-07 15:36:27 -0500

    [ADD] insert trades in database

--- src/app.py
+++ src/app.py
@@ -1,4 +1,29 @@
-from database import setup
+import database
+import logging
+import pandas
+import time
+from sqlalchemy.exc import SQLAlchemyError
+import bitso_api
+from datetime import datetime
+from schema import Trade
 
-setup()
 
+session = database.setup() # create database schema if not exists.
+
+response = bitso_api.fetch_trades()
+dataframe = pandas.DataFrame(response['payload'])
+
+def extract_trades():
+    for idx, row in dataframe.iterrows():
+        trade = Trade(id         = row.tid,
+                      book       = row.book, 
+                      maker_side = row.maker_side,
+                      amount     = row.amount,        
+                      price      = row.price, 
+                      created_at = row.created_at)
+        record = database.insert_or_update(trade)
+
+while True:
+    logging.info('######### UPDATING DATABASE AT {} #########'.format(datetime.today()))
+    extract_trades()
+    time.sleep(60)
